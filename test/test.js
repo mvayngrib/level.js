7bf251bfe32f9d54915fe12a0edfc4589e1517e2
Tim Kuijsten <tim@netsend.nl>
2016-02-12 16:42:58 +0100

    don't use idb-shim in tests

--- test/test.js
+++ test/test.js
@@ -2,9 +2,6 @@ var tape   = require('tape')
 var leveljs = require('../')
 var testCommon = require('./testCommon')
 
-// load IndexedDBShim in the tests
-require('./idb-shim.js')()
-
 var testBuffer = new Buffer('foo')
 
 /*** compatibility with basic LevelDOWN API ***/
